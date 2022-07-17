c5ad8ee90cd73ed5505170525db5bea147f0a3e0
Fernando Paulino <xizes2@hotmail.com>
2022-07-17 10:55:02 +0200

    add jest and no-plusplus:off rule to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,15 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    jest: true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: ["airbnb-base", "prettier", "jest"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
   rules: {
+    "no-plusplus": "off",
     "lines-between-class-members": [
       "error",
       "always",
